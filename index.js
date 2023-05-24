6d1ba5b427ac1e0252f32bd62665a09ce2a979b6
Chitranjan Gupta <37315561+chitranjan01@users.noreply.github.com>
2023-05-24 11:23:25 +0000

    Added reply_markup query

--- index.js
+++ index.js
@@ -76,123 +76,128 @@ app.get("/", async (req, res) => {
 })
 app.post("/", async (req, res) => {
   res.status(200).send("ok");
-  switch (req.body.message.text) {
-    case "/start":
-      {
-        const chatId = req.body.message.chat.id;
-        const message = "Hi! " + req.body.message.chat.first_name +
-          "\nWelcome You to Flipkart Product Price Tracker Bot." +
-          " You can track the price of Flipkart Product by sending the link.";
-        await sendMessage(chatId, message);
-        const data = await getUser(req.body.message.from.username);
-        if (!data) {
-          let user = new User({
-            username: req.body.message.from.username,
-            first_name: req.body.message.from.first_name,
-            last_name: req.body.message.from.last_name,
-            tid: req.body.message.from.id
-          });
-          await user.save();
-        }
-        break;
-      }
-    case "/tstart":
-      {
-        const chatId = req.body.message.chat.id;
-        const data = await getUser(req.body.message.from.username);
-        if (data) {
-          if (data.status === false) {
-            await User.findOneAndUpdate({ username: data.username }, { status: true })
-            const message = "Started Tracking...";
-            await sendMessage(chatId, message);
-            //Start Tracking
-            for (let i = 0; i < data.products.length; i++) {
-              checkPrice(data.username, data.products[i].url, data.products[i].prices ? data.products[i].prices : [-1]);
-            }
-          } else {
-            const message = "Tracking is already running.";
-            await sendMessage(chatId, message);
-          }
-        }
-        break;
-      }
-    case "/tstop":
-      {
-        const chatId = req.body.message.chat.id;
-        const data = await getUser(req.body.message.from.username);
-        if (data) {
-          if (data.status === true) {
-            await User.findOneAndUpdate({ username: data.username }, { status: false });
-            const message = "Stopped Tracking...";
-            await sendMessage(chatId, message);
-            //Stop Tracking
-            workerArray.forEach((warker) => {
-              warker.postMessage("exit");
-            })
-          } else {
-            const message = "Tracking is already stopped.";
-            await sendMessage(chatId, message);
-          }
-        }
-        break;
-      }
-    case "/list":
-      {
-        const chatId = req.body.message.chat.id;
-        const data = await getUser(req.body.message.from.username);
-        if (data) {
-          const message = "List of products you have added for tracking:";
-          await sendMessage(chatId, message);
-          for (let i = 0; i < data.products.length; i++) {
-            const product = data.products[i];
-            const productDetail = `${i + 1}. ${product.name ? product.name : "Unknown"} \nLink: ${product.url} \nCurrent Price: ${product.prices[product.prices.length - 1] ? "₹" + product.prices[product.prices.length - 1] : "Unknown"} \nStatus: ${product.status}\n`;
-            const payload = {
-              inline_keyboard:[
-                [
-                  { text:"Stop", callback_data:"stop" },
-                  { text:"Delete", callback_data:"delete"}
-                ]
-              ]
-            };
-            await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(productDetail)}&reply_markup=${encodeURIComponent(JSON.stringify(payload))}`);
-          }
-        } else {
-          const message = "You haven't added any product.";
-          await sendMessage(chatId, message);
-        }
-        break;
-      }
-    case "/help":
-      {
-        const chatId = req.body.message.chat.id;
-        const message = "Type /start to Interact with the bot.";
-        await sendMessage(chatId, message);
-        break;
-      }
-    default:
-      {
-        if (req.body.message.text.includes("https://")) {
-          const URL = req.body.message.text.match(pattern)[0];
+  const messageReceived = req.body.message;
+  if(messageReceived && messageReceived.reply_markup){
+    console.log(messageReceived.reply_markup);
+  }else{
+    switch (req.body.message.text) {
+      case "/start":
+        {
           const chatId = req.body.message.chat.id;
-          const message = "Link is Added to tracking.\n To Start Tracking send command " +
-            "/tstart and to Stop Tracking send command /tstop";
+          const message = "Hi! " + req.body.message.chat.first_name +
+            "\nWelcome You to Flipkart Product Price Tracker Bot." +
+            " You can track the price of Flipkart Product by sending the link.";
           await sendMessage(chatId, message);
           const data = await getUser(req.body.message.from.username);
-          if (data) {
-            await User.findOneAndUpdate({ username: data.username }, { $push: { products: { url: URL } } });
-          } else {
+          if (!data) {
             let user = new User({
               username: req.body.message.from.username,
               first_name: req.body.message.from.first_name,
               last_name: req.body.message.from.last_name,
-              tid: req.body.message.from.id,
-              products: [{ url: URL }]
+              tid: req.body.message.from.id
             });
             await user.save();
           }
+          break;
         }
-        break;
-      }
+      case "/tstart":
+        {
+          const chatId = req.body.message.chat.id;
+          const data = await getUser(req.body.message.from.username);
+          if (data) {
+            if (data.status === false) {
+              await User.findOneAndUpdate({ username: data.username }, { status: true })
+              const message = "Started Tracking...";
+              await sendMessage(chatId, message);
+              //Start Tracking
+              for (let i = 0; i < data.products.length; i++) {
+                checkPrice(data.username, data.products[i].url, data.products[i].prices ? data.products[i].prices : [-1]);
+              }
+            } else {
+              const message = "Tracking is already running.";
+              await sendMessage(chatId, message);
+            }
+          }
+          break;
+        }
+      case "/tstop":
+        {
+          const chatId = req.body.message.chat.id;
+          const data = await getUser(req.body.message.from.username);
+          if (data) {
+            if (data.status === true) {
+              await User.findOneAndUpdate({ username: data.username }, { status: false });
+              const message = "Stopped Tracking...";
+              await sendMessage(chatId, message);
+              //Stop Tracking
+              workerArray.forEach((warker) => {
+                warker.postMessage("exit");
+              })
+            } else {
+              const message = "Tracking is already stopped.";
+              await sendMessage(chatId, message);
+            }
+          }
+          break;
+        }
+      case "/list":
+        {
+          const chatId = req.body.message.chat.id;
+          const data = await getUser(req.body.message.from.username);
+          if (data) {
+            const message = "List of products you have added for tracking:";
+            await sendMessage(chatId, message);
+            for (let i = 0; i < data.products.length; i++) {
+              const product = data.products[i];
+              const productDetail = `${i + 1}. ${product.name ? product.name : "Unknown"} \nLink: ${product.url} \nCurrent Price: ${product.prices[product.prices.length - 1] ? "₹" + product.prices[product.prices.length - 1] : "Unknown"} \nStatus: ${product.status}\n`;
+              const payload = {
+                inline_keyboard:[
+                  [
+                    { text:"Stop", callback_data:"stop" },
+                    { text:"Delete", callback_data:"delete"}
+                  ]
+                ]
+              };
+              await axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(productDetail)}&reply_markup=${encodeURIComponent(JSON.stringify(payload))}`);
+            }
+          } else {
+            const message = "You haven't added any product.";
+            await sendMessage(chatId, message);
+          }
+          break;
+        }
+      case "/help":
+        {
+          const chatId = req.body.message.chat.id;
+          const message = "Type /start to Interact with the bot.";
+          await sendMessage(chatId, message);
+          break;
+        }
+      default:
+        {
+          if (req.body.message.text.includes("https://")) {
+            const URL = req.body.message.text.match(pattern)[0];
+            const chatId = req.body.message.chat.id;
+            const message = "Link is Added to tracking.\n To Start Tracking send command " +
+              "/tstart and to Stop Tracking send command /tstop";
+            await sendMessage(chatId, message);
+            const data = await getUser(req.body.message.from.username);
+            if (data) {
+              await User.findOneAndUpdate({ username: data.username }, { $push: { products: { url: URL } } });
+            } else {
+              let user = new User({
+                username: req.body.message.from.username,
+                first_name: req.body.message.from.first_name,
+                last_name: req.body.message.from.last_name,
+                tid: req.body.message.from.id,
+                products: [{ url: URL }]
+              });
+              await user.save();
+            }
+          }
+          break;
+        }
+    }
   }
 })
 if(process.env["BUILD"] === "Production"){
